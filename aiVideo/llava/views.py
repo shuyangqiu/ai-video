0eb6d6732c6145f93344da4b1ebac9fa8db527fe
Shuyang Qiu <qiushuya@mail.utoronto.ca>
2024-08-06 10:59:39 -0400

    added replicate

--- aiVideo/llava/views.py
+++ aiVideo/llava/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse, JsonResponse
-from rest_framework.parsers import MultiPartParser
+from django.conf import settings
 from rest_framework.views import APIView
 from .serializer import VideoSerializer
+import replicate
 
 
 class FileUploadView(APIView):
@@ -11,6 +12,16 @@ class FileUploadView(APIView):
         try:
             video = request.data['video']
             prompt = request.data['prompt']
-            return JsonResponse({'prompt': prompt})
+            output = replicate.run(
+                "nateraw/video-llava:26387f81b9417278a8578188a31cd763eb3a55ca0f3ec375bf69c713de3fb4e8",
+                input={
+                    "video_path": video,
+                    "text_prompt": prompt
+                }
+            )
+            return JsonResponse({'output': output})
         except Exception as e:
-            return JsonResponse({'error': str(e)}, status=400)
+            if settings.DEBUG:
+                return JsonResponse({'error': str(e)}, status=500)
+            else:
+                return HttpResponse(status=500)
--- aiVideo/settings.py
+++ aiVideo/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,6 +29,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# Load environment variables
+load_dotenv()
+REPLICATE_API_TOKEN = os.getenv('REPLICATE_API_TOKEN')
 
 # Application definition
 
